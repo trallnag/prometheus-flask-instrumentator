351cea07555e67b7af46702a2ddd970b253f1f04
tim.schwenke+trallnag@protonmail.ch <tim.schwenke@bayer.com>
2020-06-21 17:02:55 +0200

    Add features, fix stuff

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-https://github.com/pypa/sampleproject
-https://towardsdatascience.com/build-your-first-open-source-python-project-53471c9942a7
-"""
-
 # python setup.py sdist bdist_wheel; twine upload dist/*
 
 from setuptools import setup, find_packages
@@ -16,65 +9,33 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+requirements = ['prometheus-client', 'flask']
+
 setup(
-    name='prometheus-flask-instrumentator',  # Required
-    version='2.1.0',  # Required
+    name='prometheus-flask-instrumentator',
+    version='3.0.0',
+    description='Istruments Flask API transparently',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='prometheus instrumentation flask monitoring metrics',
+    url='https://github.com/trallnag/prometheus-flask-instrumentator',
+    author='Tim Schwenke',
+    author_email='tim.schwenke@outlook.com',
 
-    description='Istruments Flask API without being a distinct exporter',  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
-    url='https://github.com/trallnag/prometheus-flask-instrumentator',  # Optional
-    
-    author='Tim Schwenke',  # Optional
-    author_email='tim.schwenke@outlook.com',  # Optional
-
-    classifiers=[  # Optional
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
+    classifiers=[
+        'Development Status :: 4 - Beta',  # 5 - Production/Stable
         'Intended Audience :: Developers',
         'Topic :: System :: Monitoring',
         'Topic :: System :: Logging',
-
-        # Pick your license as you wish
         'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        # These classifiers are *not* checked by 'pip install'. See instead
-        # 'python_requires' below.
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
 
-    keywords='prometheus instrumentation flask monitoring metrics',  # Optional
-
-    # When your source code is in a subdirectory under the project root, e.g.
-    # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'': 'src'},  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
-    packages=find_packages(where='src'),  # Required
-
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     python_requires='>=3.5',
-
-    install_requires=[
-        'prometheus-client',
-        'flask'
-    ],  # Optional
+    install_requires=requirements,
 )
